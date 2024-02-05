ddee3b43ac2279e0d4aabad02ea4a1d198ee7f64
luyvannda <luyvannda@gmail.com>
2024-02-05 17:03:00 +0700

    practice using translate-x and translate-y

--- components/homepage/HorseImage.tsx
+++ components/homepage/HorseImage.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 export default function HorseImage() {
   return (
-    <div>
+    <div className="relative sm:translate-x-[50%] md:translate-x-0">
       <Image
         src="/horse.png"
         alt="horse"
         height={280}
         width={280}
-        className="absolute ml-10 top-12 z-10 md:right-10 md:top-20 md:w-[400px] md:h-[400px] lg:top-[50%] lg:-translate-y-1/2"
+        className="absolute top-12 z-10 
+         md:right-0 md:translate-y-[10%] md:w-[400px] md:h-[400px] lg:top-[50%] lg:-translate-y-1/2"
       />
 
       <Image
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,23 +7,14 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    screens: {
-      sm: "400px",
-
-      md: "768px",
-
-      lg: "1024px",
-
-      xl: "1280px",
-
-      "2xl": "1400px"
-    },
-
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      },
+      screens: {
+        sm: "400px"
       }
     }
   },
