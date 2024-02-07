970bf24b97f98b15e2a857b67f2bf2334de65375
luyvannda <luyvannda@gmail.com>
2024-02-07 15:52:00 +0700

    create new navigation components

---
+++ components/Navigation.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const Navigation = () => {
+  const [isRouting, setIsRouting] = useState(false);
+  const path = usePathname();
+  const [prevPath, setPrevPath] = useState("/");
+
+  useEffect(() => {
+    if (prevPath !== path) {
+      setIsRouting(true);
+    }
+  }, [path, prevPath]);
+
+  useEffect(() => {
+    if (isRouting) {
+      setPrevPath(path);
+      const timeout = setTimeout(() => {
+        setIsRouting(false);
+      }, 1200);
+      return () => clearTimeout(timeout);
+    }
+  }, [isRouting, path]);
+
+  return <div>Navigation</div>;
+};
+
+export default Navigation;
