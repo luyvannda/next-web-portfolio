94b540735333a434cfc3adb8c4b3c023b2eb019e
luyvannda <luyvannda@gmail.com>
2024-02-12 16:47:51 +0700

    use framer motion to create transition between route

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,6 +4,7 @@ import { NavLinks } from "@/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import Transition from "./animation/Transition";
 
 const Navigation = () => {
   const [isRouting, setIsRouting] = useState(false);
@@ -27,21 +28,23 @@ const Navigation = () => {
   }, [isRouting, path]);
 
   return (
-    <div className="nav_icons">
-      {/* {isRouting && <Transition />} */}
-      {NavLinks.map((nav) => (
-        <Link
-          key={nav.name}
-          href={nav.link}
-          className="mb-8 md:mb-4 min-w-[20%] xl:min-w-[15%]"
-        >
-          <nav.icon
-            className={`w-[24px] h-[24px] lg:transform lg:transition lg:duration-500 lg:ease-in-out lg:hover:-translate-y-2 ${
-              path === nav.name ? "text-purple-800" : "text-white"
-            }`}
-          />
-        </Link>
-      ))}
+    <div>
+      {isRouting && <Transition />}
+      <div className="nav_icons">
+        {NavLinks.map((nav) => (
+          <Link
+            key={nav.name}
+            href={nav.link}
+            className="mb-8 min-w-[20%] md:mb-4 xl:min-w-[15%]"
+          >
+            <nav.icon
+              className={`h-[24px] w-[24px] lg:transform lg:transition lg:duration-500 lg:ease-in-out lg:hover:-translate-y-2 ${
+                path === nav.name ? "text-purple-800" : "text-white"
+              }`}
+            />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ components/animation/Transition.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+
+const TransitionVariants = {
+  initial: {
+    y: "100%",
+    height: "100%",
+  },
+  animate: {
+    y: "0%",
+    height: "0%",
+  },
+  exit: {
+    y: ["0%", "100%"],
+    height: ["0%", "100%"],
+  },
+};
+
+const Transition = () => {
+  return (
+    <div>
+      <motion.div
+        className="fixed bottom-full right-0 z-[30] h-screen w-screen bg-[#2e2257]"
+        variants={TransitionVariants}
+        initial="initial"
+        exit="exit"
+        animate="animate"
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+      />
+      <motion.div
+        className="fixed bottom-full right-0 z-[20] h-screen w-screen bg-[#3b2d71]"
+        variants={TransitionVariants}
+        initial="initial"
+        exit="exit"
+        animate="animate"
+        transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
+      />
+      <motion.div
+        className="fixed bottom-full right-0 z-[10] h-screen w-screen bg-[#4b3792]"
+        variants={TransitionVariants}
+        initial="initial"
+        exit="exit"
+        animate="animate"
+        transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
+      />
+    </div>
+  );
+};
+
+export default Transition;
