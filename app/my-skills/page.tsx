3498d65596b20c00287e8afe6b4c044e4ae769f8
luyvannda <luyvannda@gmail.com>
2024-02-15 11:09:04 +0700

    designing my skill page

--- app/my-skills/page.tsx
+++ app/my-skills/page.tsx
@@ -1,5 +1,79 @@
+"use client";
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { SkillData } from "@/constants/skillData";
+import Image from "next/image";
+import { Autoplay } from "swiper/modules";
 
-export default function MySkillsPage() {
-  return <div>This is my skills page</div>;
-}
+const Page = () => {
+  return (
+    <div
+      style={{ backgroundImage: "url(/bg-2.jpg)" }}
+      className="flex h-screen w-screen items-center justify-center bg-cover bg-center"
+    >
+      <div className="flex max-w-[80%] flex-col items-center gap-20 text-center">
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="text-[50px] font-semibold text-white">
+            Skills{" "}
+            <span className="bg-gradient-to-r from-purple-500 to-red-500 bg-clip-text text-transparent">
+              {" "}
+              &{" "}
+            </span>
+            Technologies
+          </h1>
+          <p className="text-[20px] text-gray-400">
+            Practicing latest tech this world has to offer, one stack at a time.
+          </p>
+        </div>
+        <Swiper
+          slidesPerView={5}
+          loop={true}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+          }}
+          speed={5000}
+          modules={[Autoplay]}
+          className="max-w-[80%]"
+        >
+          {SkillData.map((skill, index) => (
+            <SwiperSlide key={index}>
+              <Image
+                src={skill.Image}
+                alt={skill.name}
+                width={skill.width}
+                height={skill.height}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <Swiper
+          slidesPerView={5}
+          loop={true}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+            reverseDirection: true,
+          }}
+          speed={5000}
+          modules={[Autoplay]}
+          className="max-w-[80%]"
+        >
+          {SkillData.map((skill, index) => (
+            <SwiperSlide key={index}>
+              <Image
+                src={skill.Image}
+                alt={skill.name}
+                width={skill.width}
+                height={skill.height}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
