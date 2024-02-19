f933bff7398322edc9183175662db92fef5519ba
luyvannda <luyvannda@gmail.com>
2024-02-19 11:03:21 +0700

    add several comments to many code snippets

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -12,20 +12,27 @@ const ProjectsPage = () => {
     >
       <div className="flex flex-col  items-center justify-center pt-32 md:pt-24">
         <div className="grid max-h-[80%] max-w-[80%] grid-cols-1 gap-5 lg:max-w-[85%] lg:grid-cols-2">
-          {Projects.map((project) => (
-            <ProjectCard
-              key={project.id}
-              id={project.id}
-              imgCover={project.imgCover}
-              title={project.title}
-              skills={project.skills}
-              description={project.description}
-              livePreview={project.livePreview}
-              sourceCode={project.sourceCode}
-            />
-          ))}
+          {/* Before we map over projects data we copy it with slice() and reverse() it so that the last data will display first */}
+
+          {Projects.slice()
+            .reverse()
+            .map((project) => (
+              <ProjectCard
+                key={project.id}
+                id={project.id}
+                imgCover={project.imgCover}
+                title={project.title}
+                skills={project.skills}
+                description={project.description}
+                livePreview={project.livePreview}
+                sourceCode={project.sourceCode}
+              />
+            ))}
         </div>
       </div>
+
+      {/* This code use to create a space between the navigation to the contents of project page */}
+
       <div className="block h-[80px] w-screen bg-transparent vs:h-[70px] xs:h-[75px] lg:h-[85px]"></div>
     </div>
   );
