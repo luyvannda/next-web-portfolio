7676957673f9a18db19d8f6a65cc211d33e1ced6
luyvannda <luyvannda@gmail.com>
2024-03-05 10:27:09 +0700

    add og and twitter card to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,23 @@ import Navigation from "../components/Navigation";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Luy Vannda",
-  description: "My first next app portfolio",
+  title: "Next Portfolio",
+  description:
+    "Welcome to my Next.js portfolio apps. This app showcase my front-end skills in Typescript, tailwind, framer motion, Swiper and more...",
+  openGraph: {
+    images: [
+      {
+        url: "https://onedrive.live.com/embed?resid=FF8AC3081FC56D69%2188623&authkey=%21AHFL2eCUsXcBlnw&width=1200&height=630",
+        width: 1200,
+        height: 630,
+        alt: "Next Portfolio App",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@luyvannda",
+  },
 };
 
 export default function RootLayout({
