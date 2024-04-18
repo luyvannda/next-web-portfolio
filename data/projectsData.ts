cd4cd7a4aa15f8f3aa751dc8eea55ffa879e1b96
luyvannda <luyvannda@gmail.com>
2024-04-18 10:55:21 +0700

    add new project to the project data and rewrite all project description

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,13 +1,13 @@
 export const Projects = [
   {
     id: 1,
-    imgCover: "web-developer-portfolio.webp",
-    title: "WEB DEVELOPER PORTFOLIO",
+    imgCover: "survey-form.webp",
+    title: "SURVEY FORM",
     skills: "HTML CSS",
     description:
-      "This is the final certification project required to pass FreeCodeCamp's responsive web design course built with HTML5 and CSS",
-    livePreview: "https://luyvannda.github.io/personal-portfolio-webpage/",
-    sourceCode: "https://github.com/luyvannda/personal-portfolio-webpage",
+      "This is the first certification project required to pass FreeCodeCamp's responsive web design course. I also collaborated with a friend in peer programming from overseas to build this project. Later, I did it again solo to certify my skill.",
+    livePreview: "https://luyvannda.github.io/survey-form/",
+    sourceCode: "https://github.com/luyvannda/survey-form",
   },
 
   {
@@ -16,20 +16,20 @@ export const Projects = [
     title: "LANDING PAGE",
     skills: "HTML CSS",
     description:
-      "This is a landing page from the Odin Project's course built with HTML5 and CSS",
+      "This landing page is a nostalgia project from The Odin Project's course, back from when I first learned about Flexbox CSS. It's where I put my flexbox skills to the test.",
     livePreview: "https://luyvannda.github.io/odin-landing-page/",
     sourceCode: "https://github.com/luyvannda/odin-landing-page",
   },
 
   {
     id: 3,
-    imgCover: "survey-form.webp",
-    title: "SURVEY FORM",
+    imgCover: "web-developer-portfolio.webp",
+    title: "WEB DEVELOPER PORTFOLIO",
     skills: "HTML CSS",
     description:
-      "This is the first certification project required to pass FreeCodeCamp's responsive web design course built with HTML5 and CSS",
-    livePreview: "https://luyvannda.github.io/survey-form/",
-    sourceCode: "https://github.com/luyvannda/survey-form",
+      "This is the fifth and final certification project required to pass FreeCodeCamp's responsive web design course. After completing 5 projects, I finally got my first certificate, yay!!!",
+    livePreview: "https://luyvannda.github.io/personal-portfolio-webpage/",
+    sourceCode: "https://github.com/luyvannda/personal-portfolio-webpage",
   },
 
   {
@@ -38,7 +38,7 @@ export const Projects = [
     title: "CALCULATOR",
     skills: "HTML CSS JAVASCRIPT",
     description:
-      "This is a calculator app, a solo project from the Odin Project's course built with HTML5, CSS and JAVASCRIPT",
+      "This calculator app is the final project from The Odin Project's Web Development Foundation course. After completing this project, I am ready to enroll in the full-stack JavaScript course next.",
     livePreview: "https://luyvannda.github.io/odin-calculator/",
     sourceCode: "https://github.com/luyvannda/odin-calculator",
   },
@@ -49,7 +49,7 @@ export const Projects = [
     title: "SIGN UP FORM",
     skills: "REACT BOOTSTRAP SASS",
     description:
-      "This sign-up-form is a project from frontend-mentor.io built with REACT, BOOTSTRAP and SASS",
+      "This sign-up-form is a project from frontend-mentor.io. This is where I put BOOTSTRAP and SASS to practice after completing BOOTSTRAP tutorial at W3Schools.",
     livePreview: "https://luyvannda.github.io/sign-up-form/",
     sourceCode: "https://github.com/luyvannda/sign-up-form/",
   },
@@ -60,7 +60,7 @@ export const Projects = [
     title: "TRAVEL JOURNAL",
     skills: "REACT BOOTSTRAP SASS",
     description:
-      "This Travel Journal is a solo project from Scrimba's React basic course built with REACT, BOOTSTRAP, and SASS",
+      "This Travel Journal is a solo project from Scrimba's React basic course by Bob Ziroll. In this project, I practiced dynamically rendering each card with React state and props.",
     livePreview: "https://luyvannda.github.io/travel-journal/",
     sourceCode: "https://github.com/luyvannda/travel-journal/",
   },
@@ -71,7 +71,7 @@ export const Projects = [
     title: "LEGACY PORTFOLIO",
     skills: "REACT BOOTSTRAP SASS JAVASCRIPT",
     description:
-      "This is my very first personal web portfolio. It was built with REACT, BOOTSTRAP, SASS, and use JAVASCRIPT to dynamically calculate my skill level based on the date I started learning each skills",
+      "This is my very first personal web portfolio. It is based on a premium design from Frontend Mentor. Additionally, it features dynamic calculation of how many days it has been since I've learned each technical skill.",
     livePreview: "https://luyvannda.github.io/",
     sourceCode: "https://github.com/luyvannda/luyvannda.github.io/",
   },
@@ -82,7 +82,7 @@ export const Projects = [
     title: "MEME GENERATOR",
     skills: "REACT BOOTSTRAP JAVASCRIPT API",
     description:
-      "This meme-generator is a solo project from Scrimba's React basic course built with REACT, BOOTSTRAP, JAVASCRIPT and fetching data from an API",
+      "This meme-generator is a solo project from Scrimba's React basic course by Bob Ziroll. This is where I learned to fetch data from an API.",
     livePreview: "https://luyvannda.github.io/meme-generator/",
     sourceCode: "https://github.com/luyvannda/meme-generator/",
   },
@@ -92,7 +92,7 @@ export const Projects = [
     title: "WORKATION RENTAL PROPERTY",
     skills: "REACT TAILWIND",
     description:
-      "This is a property rental app that is part of Tailwind Labs' official tutorial to learn and understand tailwind's fundamental built with REACT and TAILWIND",
+      "This property rental app is part of Tailwind Labs' official tutorial. This project serves as proof that I have mastered all of Tailwind's fundamentals from its creator.",
     livePreview: "https://luyvannda.github.io/workation/",
     sourceCode: "https://github.com/luyvannda/workation",
   },
@@ -103,7 +103,7 @@ export const Projects = [
     title: "PIZZA MENU",
     skills: "REACT TAILWIND TYPESCRIPT",
     description:
-      "Pizza menu is a dynamic app built with REACT, TAILWIND, and TYPESCRIPT",
+      "Pizza menu is a react app project from the highest rated React course on Udemy taught by Jonas, although the instructor taught using javascript, I creatively built this app with typescript.",
     livePreview: "https://luyvannda.github.io/pizza-menu/",
     sourceCode: "https://github.com/luyvannda/pizza-menu",
   },
@@ -114,7 +114,7 @@ export const Projects = [
     title: "TESLA LANDING PAGE",
     skills: "REACT TAILWIND TYPESCRIPT",
     description:
-      "Tesla landing page is one of a final project of Udemy's tailwind course built with REACT, TAILWIND, and TYPESCRIPT",
+      "Tesla landing page is the final project of Udemy's free tailwind course by CodeBlessYou, I have to put all my tailwind skill to a test with this one.",
     livePreview: "https://luyvannda.github.io/tesla-landing-page/",
     sourceCode: "https://github.com/luyvannda/tesla-landing-page",
   },
@@ -125,8 +125,19 @@ export const Projects = [
     title: "NEXT PORTFOLIO ",
     skills: "NEXTJS TAILWIND TYPESCRIPT AND MORE",
     description:
-      "Next Portfolio is the latest front end project that I built with NEXTJS, TAILWIND, TYPESCRIPT, FRAMER MOTION, SWIPER , REACT HOOK FORM, ETC... ",
+      "Next Portfolio is my latest Next.js project where I've learned to build it using popular front-end animation technologies such as Framer Motion, Swiper, and React Hook Form. ",
     livePreview: "https://luyvannda.vercel.app/",
     sourceCode: "https://github.com/luyvannda/next-web-portfolio",
+  },
+
+  {
+    id: 13,
+    imgCover: "odin-admin-dashboard.webp",
+    title: "ODIN ADMIN DASHBOARD ",
+    skills: "REACT TAILWIND TYPESCRIPT",
+    description:
+      "This Admin Dashboard is an assignment from The Odin Project to showcase my advanced CSS Grid skills by integrating them with Tailwind.",
+    livePreview: "https://luyvannda.github.io/odin-admin-dashboard/",
+    sourceCode: "https://github.com/luyvannda/odin-admin-dashboard",
   },
 ];
